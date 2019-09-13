7946ee356846cc343772b9ef3ff8872515bc1e88
dcaphnhs <davidcaphane@hotmail.com>
2019-09-13 09:08:57 +0100

    Updated bubble chart

--- scripts/ecds/ecds_charts.js
+++ scripts/ecds/ecds_charts.js
@@ -77,10 +77,10 @@ async function ecdsCharts() {
 	/*
     Drop any unused fields from csv:
 
-
   */
 
 	dc.renderAll();
+
 	// End of ECDS Charts
 	console.timeEnd("ProcessTime");
 }
@@ -730,7 +730,7 @@ function diagnosisCharts() {
 		.useViewBoxResizing(true)
 		.width(chtWidthStd)
 		.height(chtHeightStd)
-		// .margins({ top: 10, right: 50, bottom: 30, left: 40 })
+		.margins({ top: 0, right: 20, bottom: 40, left: 30 })
 		.dimension(dimDiagMain)
 		.group(groupDiagMain)
 		//.colors(d3.scaleOrdinal(d3SchemeCategory20b))
@@ -761,7 +761,13 @@ function diagnosisCharts() {
 		})
 		.turnOnControls()
 		.controlsUseVisibility(true);
-	// .render();
+
+	// Add axis descriptions to row charts
+
+	chtDiagMain.on("postRender", function(chart) {
+		addXLabel(chart, "Attendances");
+		addYLabel(chart, "Diagnosis Category");
+	});
 
 	const dimDiagnoses = cf.dimension(function(d) {
 			return d.Diagnoses;
@@ -993,7 +999,7 @@ function otherDetails() {
 		.margins({
 			top: 30,
 			right: 0,
-			bottom: 2, // hides x-axis labels
+			bottom: 20, // hides x-axis labels
 			left: 40
 		})
 		.transitionDuration(500)
@@ -1014,6 +1020,7 @@ function otherDetails() {
 		.xUnits(dc.units.ordinal)
 		.xAxisPadding(3)
 		.elasticX(true)
+		.xAxisLabel("GP Practice", 12)
 		// Y-Axis
 		.valueAccessor(function(d) {
 			return d.value; // y-axis, attendances
@@ -1021,6 +1028,7 @@ function otherDetails() {
 		.y(d3.scaleLinear()) // .domain([0, 20000])
 		.yAxisPadding(100) // note that padding is added to the top and bottom of the axis
 		.elasticY(true)
+		.yAxisLabel("Attendances", 12)
 		// All things radius
 		.radiusValueAccessor(function(d) {
 			const popn = practicePopn.get(practiceArr[d.key - 1]),
@@ -1094,6 +1102,22 @@ function otherDetails() {
 		})
 		.turnOnControls(true)
 		.controlsUseVisibility(true);
+
+	/*
+	Below is to remove tick marks
+	Always give xAxis and yAxis ticks separately from the Charts Definitions.
+	You cannot chain this method since subsequent methods in the stack will then be associated with the .tick call
+	https://github.com/dc-js/dc.js/issues/548
+	*/
+
+	chtGPPractice
+		.xAxis()
+		// to remove ticks
+		.tickSize(0)
+		// to blank values
+		.tickFormat(function(v) {
+			return "";
+		});
 
 	// Patient Characteristics
 
@@ -1920,3 +1944,61 @@ function pad(str, max) {
 	str = str.toString();
 	return str.length < max ? pad("0" + str, max) : str;
 }
+
+// https://www.intothevoid.io/data-visualization/row-chart-axis-labels-dc-js/
+// Functions to add x-label & y-label to Row Charts (Unsupported by dc.js)
+var addXLabel = function(chartToUpdate, displayText) {
+	var textSelection = chartToUpdate
+		.svg()
+		.append("text")
+		.attr("class", "x-axis-label")
+		.attr("text-anchor", "middle")
+		.attr("x", chartToUpdate.width() / 2)
+		.attr("y", chartToUpdate.height() - 10)
+		.text(displayText);
+	var textDims = textSelection.node().getBBox();
+	var chartMargins = chartToUpdate.margins();
+
+	// Dynamically adjust positioning after reading text dimension from DOM
+	textSelection
+		.attr(
+			"x",
+			chartMargins.left +
+				(chartToUpdate.width() -
+					chartMargins.left -
+					chartMargins.right) /
+					2
+		)
+		.attr("y", chartToUpdate.height() - Math.ceil(textDims.height) / 2);
+};
+var addYLabel = function(chartToUpdate, displayText) {
+	var textSelection = chartToUpdate
+		.svg()
+		.append("text")
+		.attr("class", "y-axis-label")
+		.attr("text-anchor", "middle")
+		.attr("transform", "rotate(-90)")
+		.attr("x", -chartToUpdate.height() / 2)
+		.attr("y", 10)
+		.text(displayText);
+	var textDims = textSelection.node().getBBox();
+	var chartMargins = chartToUpdate.margins();
+
+	// Dynamically adjust positioning after reading text dimension from DOM
+	textSelection
+		.attr(
+			"x",
+			-chartMargins.top -
+				(chartToUpdate.height() -
+					chartMargins.top -
+					chartMargins.bottom) /
+					2
+		)
+		.attr(
+			"y",
+			Math.max(
+				Math.ceil(textDims.height),
+				chartMargins.left - Math.ceil(textDims.height) - 5
+			)
+		);
+};
