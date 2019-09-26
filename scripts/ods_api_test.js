0dea83a916508cab5e77e41d5313ba8211583ab4
dcaphnhs <davidcaphane@hotmail.com>
2019-09-26 17:21:44 +0100

    Testing with the ODS API (GP Practice Names)

--- scripts/ods_api_test.js
+++ scripts/ods_api_test.js
@@ -1,9 +1,12 @@
 // https://github.com/AndersDJohnson/fetch-paginate/blob/master/README.md
 // https://digital.nhs.uk/services/organisation-data-service/guidance-for-developers/http-headers
 
+// If no need to paginate, switch to fetch
+
 let resultsMap = new Map();
 let url =
-	'https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?NonPrimaryRoleId=RO76&OrgRecordClass=RC1&Status=Active';
+	"https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?RelTypeId=RE3,RE4,RE5&TargetOrgId=03Q&RelStatus=active&Limit=1000";
+	// 'https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?NonPrimaryRoleId=RO76&OrgRecordClass=RC1&Status=Active';
 // let url1 = "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?NonPrimaryRoleId=RO76&OrgRecordClass=RC1&Status=Active&Limit=1000&Offset=1000"
 
 // const output = document.getElementById("output");
@@ -20,44 +23,49 @@ const start = window.performance.now();
 fetchPaginate
 	.default(url, {
 		items: (data) => data.Organisations,
-		params: {
-			limit: 'Limit',
-			offset: 'Offset'
-		},
-		firstOffset: 0,
-		limit: 1000,
-		offset: 1000
+		// params: {
+		// 	limit: 'Limit',
+		// 	offset: 'Offset'
+		// },
+		// firstOffset: 0,
+		// limit: 1000,
+		// offset: 1000
 	})
 	.then((data) => {
-		//console.log(data)
-		console.timeEnd('import');
-		const results = data.data;
-		// console.log(data)
-		// console.log(test)
-		console.time('loop');
+						//console.log(data)
+						console.timeEnd("import");
+						const results = data.data;
+						// console.log(results)
+						// console.log(test)
+						console.time("loop");
 
-		results.forEach((d) => {
-			const orgID = d.OrgId;
-			const orgName = d.Name;
-			let option = document.createElement('option');
-			option.value = [orgID, orgName].join(': ');
+						const entries = Object.entries(results.Organisations);
+						// console.log(entries)
 
-			if (orgID.substring(0, 3) === 'B82') {
-				outputMap.set(orgID, orgName); // add bank holiday date to the map as an integer
-				fragment.appendChild(option);
-			}
+						// entries.forEach((d) => {
+						for (var i = 0, len = entries.length; i < len; i++) {
+	
+							const orgID = entries[i][1]["OrgId"];
+							const orgName = entries[i][1]["Name"];
+							let option = document.createElement("option");
+							option.value = [orgID, orgName].join(": ");
 
-			dataListPractice.append(fragment);
-		});
-		console.timeEnd('loop');
-		console.log('Ready!');
-		const end = window.performance.now();
-		const timeTaken = end - start;
-		readyDiv.innerHTML =
-			'Ready. Time taken to load: ' +
-			+(timeTaken / 1000).toFixed(1) +
-			'sec';
-	});
+							if (orgID.substring(0, 2) === "B8") {
+								outputMap.set(orgID, orgName); // add bank holiday date to the map as an integer
+								fragment.appendChild(option);
+							}
+
+							dataListPractice.append(fragment);
+						};
+						console.timeEnd("loop");
+						console.log("Ready!");
+						const end = window.performance.now();
+						const timeTaken = end - start;
+						readyDiv.innerHTML =
+							"Ready. Time taken to load: " +
+							+(timeTaken / 1000).toFixed(1) +
+							"sec";
+					});
 
 /*
 selectBox.addEventListener('input', function() {
