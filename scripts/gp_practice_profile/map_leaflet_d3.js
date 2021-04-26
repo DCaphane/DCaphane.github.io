5b24d1c8c781e993ca96c8703e6eeefe1e837bc9
D Caph <davidcaphane@hotmail.com>
2021-04-26 20:39:40 +0100

    D3 Circles on Leaflet

--- scripts/gp_practice_profile/map_leaflet_d3.js
+++ scripts/gp_practice_profile/map_leaflet_d3.js
@@ -1,3 +1,12 @@
+/*
+
+Drawing points of interest using this demo:
+  https://chewett.co.uk/blog/1030/overlaying-geo-data-leaflet-version-1-3-d3-js-version-4/
+
+
+
+*/
+
 const mapD3 = {
   map: mapInitialise.mapInit("mapIMDD3"),
   scaleBar: mapInitialise.scaleBar("bottomleft"),
@@ -19,9 +28,11 @@ mapD3.map.getPane("lsoaBoundaryPane").style.zIndex = 375;
 mapD3.map.createPane("ccgBoundaryPane");
 mapD3.map.getPane("ccgBoundaryPane").style.zIndex = 374;
 
-
 let imdDomainDescD3 = "IMD Rank",
   imdDomainShortD3 = "imdRank";
+
+// use for temp demp only to center in London
+mapD3.map.setView([51.505548, -0.075316], 16);
 
 (function imdDomainD3(id = "selD3Leaf") {
   // https://gist.github.com/lstefano71/21d1770f4ef050c7e52402b59281c1a0
@@ -60,8 +71,85 @@ let imdDomainDescD3 = "IMD Rank",
     console.log(imdDomainShortD3);
     // recolourIMDLayer(imdDomainShort);
   });
-})();
 
+  // add SVG to Leaflet map via Leaflet
+  const svgLayer = L.svg();
+  svgLayer.addTo(mapD3.map);
+
+  // by default, Leaflet adds an initial g element inside this svg
+  // both the SVG and g can be accessed via D3
+  const svg = d3.select("#mapIMDD3").select("svg");
+  const g = svg.select("g");
+
+  const pointsOfInterest = [
+    { coordinate: [51.506532, -0.074624] },
+    { coordinate: [51.504452, -0.076028] },
+    { coordinate: [51.505548, -0.075316] },
+    { coordinate: [51.508109, -0.076061] },
+    { coordinate: [51.506019, -0.073639] },
+  ];
+
+  pointsOfInterest.forEach(function (d) {
+    d.latLong = new L.LatLng(d.coordinate[0], d.coordinate[1]);
+  });
+  // console.log(pointsOfInterest)
+
+  var feature = g
+    .selectAll("circle")
+    .data(pointsOfInterest)
+    .enter()
+    .append("circle")
+    .style("stroke", "black")
+    .style("opacity", 0.4)
+    .style("fill", "blue")
+    .attr("r", 10);
+
+  function drawAndUpdateCircles() {
+    feature.attr("transform", function (d) {
+      var layerPoint = mapD3.map.latLngToLayerPoint(d.latLong);
+      return "translate(" + layerPoint.x + "," + layerPoint.y + ")";
+    });
+  }
+
+  drawAndUpdateCircles();
+  mapD3.map.on("moveend", drawAndUpdateCircles);
+
+  geoDataLsoaBoundaries.then(function (v) {
+    const projection = d3.geoMercator();
+    const geoGenerator = d3.geoPath().projection(projection);
+
+    // v is the full dataset
+    // console.log(v)
+
+    // v.features.forEach(function (d) {
+    //   console.log(d)
+    // const a = geoGenerator(d)
+    // console.log(a)
+    // })
+
+    // const u = g
+    //   .selectAll('path')
+    // .data(v.features)
+
+    // u.enter()
+    //   .append('path')
+    //   .attr('d', geoGenerator)
+    // .attr('class', "d3-map")
+
+    // const transform = d3.geoTransform({ point: projectPoint }),
+    // path = d3.geoPath().projection(transform)
+
+    // const d3_features = g.selectAll('path')
+    //   .data(v.features)
+    //   .enter()
+    // .append('path')
+
+    // function projectPoint(x, y) {
+    //   let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    //   this.stream.point(point.x, point.y);
+    // }
+  });
+})();
 
 const baseTreeD3Leaf = (function () {
   const defaultBasemap = L.tileLayer
