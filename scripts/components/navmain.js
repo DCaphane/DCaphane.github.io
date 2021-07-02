786aa966420020f34ccd9e8341296de60947eb99
D Caph <davidcaphane@hotmail.com>
2021-07-02 22:18:27 +0100

    Refactored navbar Main and Side

--- scripts/components/navmain.js
+++ scripts/components/navmain.js
@@ -1,171 +1,189 @@
 // https://javascript.info/modifying-document#a-word-about-document-write
 // https://plnkr.co/edit/LJbzzvqdwGaf2iojgymr?p=preview
 
-// Object consisting of main headers and a subsequent array of nav link and subsequent links
-const dataNavMain = {
-	Home: ["index.html", {}],
-	Navigation: [
-		"#",
-		{
-			"dc Demos": [
-				"#",
-				{
-					ECDS: ["ecds.html", {}],
-					Referrals: ["referrals.html", {}],
-				}
-			],
-			"Map Demos": [
-				"#",
-				{
-					PCNs: ["primary_care_networks.html", {}],
-					"GP Profiles": ["gp_practice_profile.html", {}],
-				}
-			],
-			"D3 Learning": [
-				"#",
-				{
-					"Nav 3a": ["d3_learning_colours.html", {}],
-					"Nav 3b": ["d3_learning_axis.html", {}],
-          "Nav 3c": ["d3_learning_canvas.html", {}],
-          "Nav 3d": ["#", {}]
-				}
-			],
-			"Nav 4": [
-				"#",
-				{
-					"Nav 4a": ["#", {}],
-					"Nav 4b": ["#", {}],
-					"Nav 4c": ["#", {}]
-				}
-			],
-			"Nav 5": ["#", {}]
-		}
-	],
-	FAQ: ["#", {}],
-	Blog: [
-		"#",
-		{
-			"Blog 1": [
-				"#",
-				{
-					"Blog 1a": ["#", {}],
-					"Blog 1b": ["#", {}]
-				}
-			]
-		}
-	],
-	"About Us": ["about_us.html", { "Sub Contact": ["#", {}] }]
-};
+(function createNavBarMain() {
+  // Object consisting of main headers and a subsequent array of nav link and subsequent links
+  const dataNavMain = {
+    Home: ["index.html", {}],
+    Navigation: [
+      "#",
+      {
+        "dc Demos": [
+          "#",
+          {
+            ECDS: ["ecds.html", {}],
+            Referrals: ["referrals.html", {}],
+          },
+        ],
+        "Map Demos": [
+          "#",
+          {
+            PCNs: ["primary_care_networks.html", {}],
+            "GP Profiles": ["gp_practice_profile.html", {}],
+          },
+        ],
+        "D3 Learning": [
+          "#",
+          {
+            "Nav 3a": ["d3_learning_colours.html", {}],
+            "Nav 3b": ["d3_learning_axis.html", {}],
+            "Nav 3c": ["d3_learning_canvas.html", {}],
+            "Nav 3d": ["#", {}],
+          },
+        ],
+        "Nav 4": [
+          "#",
+          {
+            "Nav 4a": ["#", {}],
+            "Nav 4b": ["#", {}],
+            "Nav 4c": ["#", {}],
+          },
+        ],
+        "Nav 5": ["#", {}],
+      },
+    ],
+    FAQ: ["#", {}],
+    Blog: [
+      "#",
+      {
+        "Blog 1": [
+          "#",
+          {
+            "Blog 1a": ["#", {}],
+            "Blog 1b": ["#", {}],
+          },
+        ],
+      },
+    ],
+    "About Us": ["about_us.html", { "Sub Contact": ["#", {}] }],
+  };
 
-// Tried using appendChild but this replaces existing nodes.
-// Use insertAdjacentHTML instead. This requires element as text string.
-// Down and Right arrows used to identify sub menus (Down for first level, right for all subsequent levels)
-const spanArrowDown = document.createElement("span");
-spanArrowDown.className = "fas fa-angle-down";
-const textSpanArrowDown = spanArrowDown.outerHTML;
+  // To keep track of recursion depth
+  let counter = 0,
+    itemNumber = 0,
+    trackKey = 0,
+    headingPosition = 0;
 
-const spanArrowRight = document.createElement("span");
-spanArrowRight.classList = "fas fa-angle-right";
-const textSpanArrowRight = spanArrowRight.outerHTML;
+  const arrDataKeys = Object.keys(dataNavMain), // An array of the top level headings only
+    dataKeysCount = arrDataKeys.length;
+  // lastItemText = arrDataKeys[arrDataKeys.length - 1];
 
-let counter = 0;
-const dataKeysCount = Object.keys(dataNavMain).length;
+  // Tried using appendChild but this replaces existing nodes.
+  // Use insertAdjacentHTML instead. This requires element as text string.
+  // Down and Right arrows used to identify sub menus (Down for first level, right for all subsequent levels)
+  const spanArrowDown = document.createElement("span");
+  spanArrowDown.className = "fa-solid fa-angle-down";
+  const textSpanArrowDown = spanArrowDown.outerHTML;
 
-const navMain = document.getElementById("nav-main");
-const container = document.createDocumentFragment();
+  const spanArrowRight = document.createElement("span");
+  spanArrowRight.classList = "fa-solid fa-angle-right";
+  const textSpanArrowRight = spanArrowRight.outerHTML;
 
-// The label and input will create our responsive button that becomes visible instead of the menu in small screen sizes
-const label = document.createElement("label");
-label.className = "responsive-button";
-label.setAttribute("for", "responsive-button");
+  const navMain = document.getElementById("nav-main");
+  const container = document.createDocumentFragment();
 
-const new_i = document.createElement("i");
-new_i.className = "fas fa-bars";
+  // The label and input will create our responsive button that becomes visible instead of the menu in small screen sizes
+  const label = document.createElement("label");
+  label.className = "responsive-button";
+  label.setAttribute("for", "responsive-button");
 
-label.append(new_i);
-container.prepend(label);
+  const new_i = document.createElement("i");
+  new_i.className = "fa-solid fa-bars";
 
-const input = document.createElement("input");
-input.setAttribute("type", "checkbox");
-input.setAttribute("role", "button");
-input.id = "responsive-button";
-container.append(input);
+  label.append(new_i);
+  container.prepend(label);
 
-createTree(container, dataNavMain);
-navMain.append(container);
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("role", "button");
+  input.setAttribute("id", "responsive-button");
+  container.append(input);
 
-// The below is used to highlight the active page
-// elemActive is declared in the calling page
-let elem = document.getElementById(elemActive);
-elem.className += " " + "active";
+  createTree(container, dataNavMain);
+  navMain.append(container);
 
-container.addEventListener("click", function() {
-  this.classList.toggle("active");
-});
+  // The below is used to highlight the active page
+  // elemActive is declared in the calling page
+  let elem = document.getElementById(elemActive);
+  elem.classList.add("active");
 
-function createTree(container, obj) {
-  container.append(createTreeDom(obj));
-}
+  container.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
 
-function createTreeDom(obj, subCounter) {
-  // if there's no children, then the call returns undefined
-  // and the <ul> won't be created
-  if (!Object.keys(obj).length) return;
-
-  const ul = document.createElement("ul");
-  if (counter === 0) {
-    ul.className = "nav-main";
+  function createTree(container, obj) {
+    container.append(createTreeDom(obj));
   }
 
-  if (typeof subCounter === "number") {
-    // subCounter is used to identify recursion depth - ie. heading level
-    subCounter++;
-  } else {
-    subCounter = 0; // initialise
-  }
+  function createTreeDom(obj, depth) {
+    // console.log({
+    //   key: trackKey, // navbar text
+    //   headingPosition: headingPosition, // This is the main heading order ie 1st, 2nd...
+    //   depth: depth, // 0 is a main heading, 1 is sub heading. 2 is sub heading of 1 etc
+    //   // itemNumber: itemNumber, // a counter of each item
+    // });
 
-  for (let key in obj) {
-    const li = document.createElement("li");
+    // if there are no children, then the call returns undefined and the <ul> won't be created
+    if (!Object.keys(obj).length) return;
 
-    // add an id to the main headings only - this can then be selected to highlight as class active
-    if (subCounter === 0) {
-      li.setAttribute("id", key.split(" ").join("-")); // remove any spaces and replace with a hyphen
-      if (counter === dataKeysCount + 1) { // Changed the navbar and needed to add +1, not sure why...
-        // Used to identify the last main item which will have a class applied to align right
-        // this was previously done is css but trickier with multi level headings
-        li.className += " " + "last-item";
+    const ul = document.createElement("ul");
+    if (counter === 0) {
+      counter++;
+      ul.className = "nav-main";
+    }
+
+    if (typeof depth === "number") {
+      // depth is used to identify recursion depth - ie. heading level
+      depth++;
+    } else {
+      depth = 0; // initialise
+    }
+
+    for (let key in obj) {
+      trackKey = key;
+      itemNumber++;
+      const li = document.createElement("li");
+
+      // add an id to the main headings only - this can then be selected to highlight as class active
+      if (depth === 0) {
+        headingPosition++;
+        li.setAttribute("id", key.split(" ").join("-")); // remove any spaces and replace with a hyphen
+
+        if (headingPosition === dataKeysCount) {
+          // Used to identify the last main item which will have a class applied to align right
+          // this was previously done is css but trickier with multi level headings
+          li.classList.add("last-item");
+        }
       }
-    }
 
-    const anchor = document.createElement("a");
-    // <a href='#...'>key text</a>
-    anchor.setAttribute("href", obj[key][0]);
-    anchor.innerHTML = key;
+      const anchor = document.createElement("a");
+      // <a href='#...'>key text</a>
+      anchor.setAttribute("href", obj[key][0]);
+      anchor.innerHTML = key;
 
-    let childrenUl = createTreeDom(obj[key][1], subCounter);
+      let childrenUl = createTreeDom(obj[key][1], depth);
 
-    if (childrenUl) {
-      // Does the menu heading have any sub-menus
-      // if so, add a has-childnre class
-      li.className += " " + "has-children";
-      // then, add an arrow pointing down for sub menu off first level or pointing right for all subsequent levels.
-      if (subCounter === 0) {
-        // appendChild replaces any existing anchor so use insertAdjacentHTML
-        anchor.insertAdjacentHTML("beforeend", textSpanArrowDown);
-      } else {
-        anchor.insertAdjacentHTML("beforeend", textSpanArrowRight);
+      if (childrenUl) {
+        // Does the menu heading have any sub-menus
+        // if so, add a has-children class
+        li.classList.add("has-children");
+        // then, add an arrow pointing down for sub menu off first level or pointing right for all subsequent levels.
+        if (depth === 0) {
+          // appendChild replaces any existing anchor so use insertAdjacentHTML
+          anchor.insertAdjacentHTML("beforeend", textSpanArrowDown);
+        } else {
+          anchor.insertAdjacentHTML("beforeend", textSpanArrowRight);
+        }
       }
-    }
-    li.append(anchor);
+      li.append(anchor);
 
-    if (childrenUl) {
-      li.append(childrenUl);
+      if (childrenUl) {
+        li.append(childrenUl);
+      }
+
+      ul.append(li);
     }
 
-    ul.append(li);
+    return ul;
   }
-
-  // Used to identify first pass...
-  counter++;
-  return ul;
-}
+})();
--- scripts/components/navside.js
+++ scripts/components/navside.js
@@ -1,33 +1,133 @@
-//
-const containerNavSide = document.getElementById("article-left");
+(function createNavBarSide() {
+  const containerNavSide = document.getElementById("article-left");
 
-const ul = document.createElement("ul");
-ul.className = "nav-side";
-ul.id = "side-nav";
+  const ul = document.createElement("ul");
+  ul.className = "nav-side";
+  ul.id = "side-nav";
 
-let sections = document.querySelectorAll("#article-right section");
-let i = 0;
-for (let section of sections) {
-  const li = document.createElement("li");
-  if (i === 0) {
-    li.className = "active";
+  let sections = document.querySelectorAll("#article-right section");
+  let i = 0;
+  for (let section of sections) {
+    const li = document.createElement("li");
+    if (i === 0) {
+      li.className = "active";
+    }
+    const anchor = document.createElement("a");
+
+    anchor.setAttribute("href", "#" + section.id);
+    let str = section.querySelectorAll("h2")[0].innerText;
+    anchor.innerHTML = str; // .substring(0, 15); originally used to truncate long text
+
+    li.appendChild(anchor);
+
+    ul.appendChild(li);
+    i += 1;
+    /*
+      // anchor
+      console.log(section.id)
+      // text to display
+      console.log(section.querySelectorAll('h2')[0].innerText);
+      */
   }
-  const anchor = document.createElement("a");
 
-  anchor.setAttribute("href", "#" + section.id);
-  let str = section.querySelectorAll("h2")[0].innerText;
-  anchor.innerHTML = str // .substring(0, 15); originally used to truncate long text
+  containerNavSide.append(ul);
+})();
 
-  li.appendChild(anchor);
+// https://jsfiddle.net/mekwall/up4nu/
+// Cache selectors
 
-  ul.appendChild(li);
-  i += 1;
-  /*
-    // anchor
-    console.log(section.id)
-    // text to display
-    console.log(section.querySelectorAll('h2')[0].innerText);
-    */
+let mainElem = document.getElementById("article-right"),
+  mainElemCoords,
+  navItems = document.getElementById("side-nav"),
+  itemsList = navItems.getElementsByTagName("li"),
+  store = {}, // object to store id and top position within section
+  mainItem;
+
+// The click event is applied to the <li></li>, not the <a>
+for (let item of itemsList) {
+  item.addEventListener("click", function () {
+    // https://javascript.info/styles-and-classes
+    // toggle selected state on/ off
+    let current = document.querySelector("#side-nav > li.active");
+
+    if (current !== null) {
+      // possible that there might not be an active class - returns null
+      current.classList.remove("active");
+    }
+    // }
+    this.classList.add("active");
+  });
 }
 
-containerNavSide.append(ul);
+// Bind to scroll
+// mainElem.addEventListener("scroll", function(e) {
+// window.addEventListener("scroll", function(e) {
+// testing only
+//console.log("Yscroll: " + window.scrollY);
+// let bottom = document.getElementById("nav-main");
+//console.log("btm: " + bottom.getBoundingClientRect().bottom);
+// });
+
+// https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+// Reference: http://www.html5rocks.com/en/tutorials/speed/animations/
+
+let last_known_scroll_position = 0,
+  ticking = false;
+
+function navHighlight(scroll_pos) {
+  // do something with the scroll position
+  for (let item of itemsList) {
+    let str = item.children[0].getAttribute("href"), // the <a> element contains href attr
+      coordsSection,
+      topPosition,
+      bottomPosition;
+
+    const navMainHeight = 60,
+      navMainTransitionAreaLower = navMainHeight * 2,
+      navMainTransitionAreaUpper = navMainHeight * 3;
+
+    // below was used when overscroll on separate element
+    // mainElemCoords = 0; // 'mainElem.getBoundingClientRect(); //getCoords(mainElem);
+
+    mainItem = mainElem.querySelector(str); //querySelectorAll
+    coordsSection = mainItem.getBoundingClientRect();
+
+    topPosition = coordsSection.top; // - mainElemCoords.top;
+    bottomPosition = coordsSection.bottom; // - mainElemCoords.top;
+    // console.log(str + ": " + topPosition + " - " + bottomPosition);
+
+    if (
+      topPosition <= navMainTransitionAreaUpper &&
+      bottomPosition > navMainTransitionAreaUpper
+    ) {
+      item.classList.add("active");
+      item.classList.remove("semi-active");
+    } else if (
+      topPosition > navMainHeight &&
+      topPosition <= navMainTransitionAreaLower
+    ) {
+      item.classList.add("active");
+    } else if (
+      bottomPosition > navMainTransitionAreaLower &&
+      bottomPosition <= navMainTransitionAreaUpper
+    ) {
+      item.classList.remove("active");
+      item.classList.add("semi-active");
+    } else {
+      item.classList.remove("active", "semi-active");
+    }
+  }
+}
+
+window.addEventListener("scroll", function (e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      navHighlight(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
